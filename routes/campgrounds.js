7d22e49edc0ee83f961530402b4e7346f5dab2fd
cindyzacarias <cindyjzacarias@gmail.com>
2023-05-27 22:48:09 -0400

    Success Partial

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -46,12 +46,14 @@ router.get('/:id/edit', async (req, res) => {
 router.put('/:id', validateCampground, catchAsync (async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 router.delete('/:id', async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campgrounds');
     res.redirect('/campgrounds');
 });
 
