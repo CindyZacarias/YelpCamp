317e7cc19bf91dd24f0b684e7dc80d853bca62e0
cindyzacarias <cindyjzacarias@gmail.com>
2023-06-14 21:22:34 -0400

    Login Routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 const catchAsync = require('../utilities/catchAsync');
 const User = require('../models/user');
 const passport = require('passport');
